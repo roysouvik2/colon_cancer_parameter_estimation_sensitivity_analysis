a0a57ceb02245b69a7cba046d7d3e7ce54f903d0
Souvik Roy <1roysouvik@gmail.com>
2020-07-03 11:40:24 -0500

    Add files via upload

--- parameter_estimation/DRIVER.m
+++ parameter_estimation/DRIVER.m
@@ -3,6 +3,7 @@
 % Authors: Achyuth Manoj, Susanth Kakarla, Suvra Pal and Souvik Roy
 
 clear all
+clc
 
 % Generating data
 [samples_index,Td,Nd,Ld,Cd] = data();
@@ -11,4 +12,9 @@ clear all
 theta = ncg(samples_index,Td,Nd,Ld,Cd);
 
 
-theta
+fprintf('\n\nThe value of d is %.6e\n',theta(1));
+fprintf('The value of l is %.6e\n',theta(2));
+fprintf('The value of s is %.6e\n',theta(3));
+fprintf('The value of p is %.6e\n',theta(4));
+fprintf('The value of k is %.6e\n',theta(5));
+fprintf('The value of q is %.6e\n',theta(6));
